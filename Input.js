716c0cfa1eda9a429835f6662104b8cd301dc0e3
Thoraxebe <kevin.van.den.bosch.b@gmail.com>
2026-01-12 21:35:08 +0100

    Update Input.js

--- Input.js
+++ Input.js
@@ -1,9 +1,6 @@
-
 const modifier = (text) => {
-
   try { if (typeof ATS_timeCommand === 'function') text = ATS_timeCommand(text); } catch (_) {}
   try { if (typeof ATS_onInput === 'function') text = ATS_onInput(text); } catch (_) {}
-
   return { text };
 };
 modifier(text);
