47f26f8b119b8fa59b5795f3269c77f0cfec8148
Thoraxebe <kevin.van.den.bosch.b@gmail.com>
2026-01-13 21:06:10 +0100

    Update Input.js

--- Input.js
+++ Input.js
@@ -1,8 +1,11 @@
-
 const modifier = (text) => {
 
   try { if (typeof ATS_timeCommand === 'function') text = ATS_timeCommand(text); } catch (_) {}
-  try { if (typeof ATS_onInput === 'function') text = ATS_onInput(text); } catch (_) {}
+
+  // Skip natural language time parsing if a slash command was already handled
+  if (!ATS.cmd || !ATS.cmd.suppressLLM) {
+    try { if (typeof ATS_onInput === 'function') text = ATS_onInput(text); } catch (_) {}
+  }
 
   return { text };
 };
